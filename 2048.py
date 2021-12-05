9966b0762467e1e513ddd1f1271b0252bbabaeb3
CorleoneH <longhuang0720@gmail.com>
2021-12-05 17:31:00 +0800

    Improve stability: Add input WIN GOAL exception handing
    Signed-off-by: CorleoneH <longhuang0720@gmail.com>

--- 2048.py
+++ 2048.py
@@ -6,37 +6,6 @@ import platform
 import random
 import itertools
 
-def trim(seqs, direction=0):
-    return ([0, 0, 0, 0] + [n for n in seqs if n])[-4:] if direction else ([n for n in seqs if n] + [0, 0, 0, 0])[:4]
-
-def sum_seqs(seqs, direction=0):
-    if seqs[1] and seqs[2] and seqs[1] == seqs[2]:
-        return trim([seqs[0], seqs[1]*2, 0, seqs[3]], direction=direction)
-    if seqs[0] and seqs[1] and seqs[0] == seqs[1]:
-        seqs[0], seqs[1] = seqs[0]*2, 0
-    if seqs[2] and seqs[3] and seqs[2] == seqs[3]:
-        seqs[2], seqs[3] = seqs[2]*2, 0
-    return trim(seqs, direction=direction)
-        
-
-def up(grid):
-    for col in [0, 1, 2, 3]:
-        for _idx, n in enumerate(sum_seqs(trim([row[col] for row in grid]))):
-            grid[_idx][col] = n
-    return grid    
-    
-def down(grid):
-    for col in [0, 1, 2, 3]:
-        for _idx, n in enumerate(sum_seqs(trim([row[col] for row in grid], direction=1), direction=1)):
-            grid[_idx][col] = n
-    return grid  
-
-def left(grid):
-    return [sum_seqs(trim(row)) for row in grid]
-
-def right(grid):
-    return [sum_seqs(trim(row, direction=1), direction=1) for row in grid]
-
 class Game:
     grid = []
     controls = ['w', 'a', 's', 'd']
@@ -44,9 +13,53 @@ class Game:
         self.win_goal = win_goal
         self.cur_goal = 0
 
+    @staticmethod
+    def trim(seqs, direction=0):
+        return ([0, 0, 0, 0] + [n for n in seqs if n])[-4:] if direction else ([n for n in seqs if n] + [0, 0, 0, 0])[:4]
+
+    @staticmethod
+    def sum_seqs(seqs, direction=0):
+        if seqs[1] and seqs[2] and seqs[1] == seqs[2]:
+            return Game.trim([seqs[0], seqs[1]*2, 0, seqs[3]], direction=direction)
+        if seqs[0] and seqs[1] and seqs[0] == seqs[1]:
+            seqs[0], seqs[1] = seqs[0]*2, 0
+        if seqs[2] and seqs[3] and seqs[2] == seqs[3]:
+            seqs[2], seqs[3] = seqs[2]*2, 0
+        return Game.trim(seqs, direction=direction)
+            
+    @staticmethod
+    def up(grid):
+        for col in [0, 1, 2, 3]:
+            for _idx, n in enumerate(Game.sum_seqs(Game.trim([row[col] for row in grid]))):
+                grid[_idx][col] = n
+        return grid    
+
+    @staticmethod    
+    def down(grid):
+        for col in [0, 1, 2, 3]:
+            for _idx, n in enumerate(Game.sum_seqs(Game.trim([row[col] for row in grid], direction=1), direction=1)):
+                grid[_idx][col] = n
+        return grid  
+
+    @staticmethod
+    def left(grid):
+        return [Game.sum_seqs(Game.trim(row)) for row in grid]
+    
+    @staticmethod
+    def right(grid):
+        return [Game.sum_seqs(Game.trim(row, direction=1), direction=1) for row in grid]
+    
     def set_win_goal(self):
         self.clean_screen()
-        self.win_goal = int(input("Input your win goal: "))
+        while True:
+            win_goal = input("Input your win goal[Enter to default:32]: ")
+            if len(win_goal) == 0:
+                break
+            try:
+                self.win_goal = int(win_goal)
+                break
+            except ValueError:
+                print("Oops! That was no valid number. Try again...")
 
     def get_cur_goal(self):
         self.cur_goal = max(max(row) for row in self.grid)
@@ -81,7 +94,7 @@ class Game:
         # --------------- print game main zone [end]----------------------------
 
     def logic(self, control):
-        grid = {'w' : up, 's' : down, 'a' : left, 'd' : right}[control]([[c for c in r] for r in self.grid])
+        grid = {'w' : self.up, 's' : self.down, 'a' : self.left, 'd' : self.right}[control]([[c for c in r] for r in self.grid])
         if grid != self.grid:
             del self.grid[:]
             self.grid.extend(grid)
@@ -90,7 +103,7 @@ class Game:
             self.rnd_field()
             self.print_screen()
         else:
-            if not [1 for g in [f(grid) for f in [up, down, left, right]] if g != self.grid]:
+            if not [1 for g in [f(grid) for f in [self.up, self.down, self.left, self.right]] if g != self.grid]:
                 return -1, "Game Over!!!"
 
         return 0, ''    # return 0 if continue; return 1 if "Win", return -1 if "Game Over"
